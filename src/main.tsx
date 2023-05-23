66166e5e2a7ba46c9ef2352093dbf1f2eb2d8d31
Valderrama20 <jose23122009@gmai.com>
2023-05-22 23:24:04 -0300

    home casi listo

--- src/main.tsx
+++ src/main.tsx
@@ -2,24 +2,22 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
-import DAYS from './utils/days.ts'
 import {
   createBrowserRouter,
   RouterProvider
 } from "react-router-dom"
 
 
+// let array = [{
+//   path: "/",
+//   element: <App/>
+// }]
 
-let array = [{
-  path: "/",
-  element: <App/>
-}]
-
-for(let i = 1 ; i<50 ; i++) {
-  const obj = {
-    path: `/day-${i}`,
-  }
-}
+// for(let i = 1 ; i<50 ; i++) {
+//   const obj = {
+//     path: `/day-${i}`,
+//   }
+// }
 
 const router = createBrowserRouter([
   {
@@ -27,8 +25,7 @@ const router = createBrowserRouter([
     element: <App/>
   },
   {
-    path: "/jose",
-    element: <h1>jose</h1>
+    path: "/dia-1",
   }
 ])
 
